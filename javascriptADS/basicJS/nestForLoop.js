9929fd47acb5515639f3f569734c90124eff101f
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-07-24 11:54:27 +0800

    Nesting For LoopsPassed

---
+++ javascriptADS/basicJS/nestForLoop.js
@@ -0,0 +1,9 @@
+function multiplyAll(arr) {
+    var product = arr;
+    // Only change code below this line
+  
+    // Only change code above this line
+    return product;
+  }
+  
+  multiplyAll([[1,2],[3,4],[5,6,7]]);
