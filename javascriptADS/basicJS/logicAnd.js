a56a667ede1f161ff62db45e16da3edf4ed79baa
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-07-24 11:18:34 +0800

    Comparisons with the Logical And Operator

---
+++ javascriptADS/basicJS/logicAnd.js
@@ -0,0 +1,14 @@
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val > 5) {
+    if (val < 10) {
+      return "Yes";
+    }
+  }
+
+  // Only change code above this line
+  return "No";
+}
+
+testLogicalAnd(10);
