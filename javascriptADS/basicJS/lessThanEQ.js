75e0ea5d820e13dfcbacd19f9669588358a941d4
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-07-24 11:17:29 +0800

    Comparison with the Less Than Or Equal To Operator

---
+++ javascriptADS/basicJS/lessThanEQ.js
@@ -0,0 +1,13 @@
+function testLessOrEqual(val) {
+  if (val) {  // Change this line
+    return "Smaller Than or Equal to 12";
+  }
+
+  if (val) {  // Change this line
+    return "Smaller Than or Equal to 24";
+  }
+
+  return "More Than 24";
+}
+
+testLessOrEqual(10);
