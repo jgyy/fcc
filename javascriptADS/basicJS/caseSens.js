6214349f118239efef4a0576dfc6705497384047
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-07-24 10:04:45 +0800

    Understanding Case Sensitivity in Variables

---
+++ javascriptADS/basicJS/caseSens.js
@@ -0,0 +1,9 @@
+// Variable declarations
+var StUdLyCapVaR;
+var properCamelCase;
+var TitleCaseOver;
+
+// Variable assignments
+StUdLyCapVaR = 10;
+properCamelCase = "A String";
+TitleCaseOver = 9000;
